4d2207ad1a2faef45d4182c0fb6f303ce73b625d
Tosin Onikute <tosinmath007@gmail.com>
2024-03-09 04:49:57 +0000

    Gradle setup
    * Import compose
    * Set Kotlin version
    * Add Dagger Hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,25 @@
 plugins {
-    //trick: for the same plugin versions in all sub-modules
+    kotlin("multiplatform").version("1.8.21").apply(false)
     id("com.android.application").version("8.1.0-beta01").apply(false)
     id("com.android.library").version("8.1.0-beta01").apply(false)
     kotlin("android").version("1.8.21").apply(false)
-    kotlin("multiplatform").version("1.8.21").apply(false)
+    id("org.jetbrains.compose").apply(false)
 }
-
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.squareup.sqldelight:gradle-plugin:1.5.5")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.17.3")
+    }
+}
+allprojects {
+    apply(plugin = "kotlinx-atomicfu")
+}
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
