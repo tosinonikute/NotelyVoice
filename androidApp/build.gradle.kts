14f5574efa419e2c0923241e7898c3d62f09c25d
Tosin Onikute <tosinmath007@gmail.com>
2024-03-09 04:55:16 +0000

    Gradle setup
    * Import Hilt
    * Import Navigation Compose

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("org.jetbrains.compose")
 }
 
 android {
@@ -46,4 +49,10 @@ dependencies {
     implementation("androidx.compose.foundation:foundation:1.4.3")
     implementation("androidx.compose.material:material:1.4.3")
     implementation("androidx.activity:activity-compose:1.7.1")
+
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("multiplatform").version("1.8.21").apply(false)
+    kotlin("multiplatform").version("1.8.20").apply(false)
     id("com.android.application").version("8.1.0-beta01").apply(false)
     id("com.android.library").version("8.1.0-beta01").apply(false)
-    kotlin("android").version("1.8.21").apply(false)
+    kotlin("android").version("1.8.20").apply(false)
     id("org.jetbrains.compose").apply(false)
 }
 buildscript {
