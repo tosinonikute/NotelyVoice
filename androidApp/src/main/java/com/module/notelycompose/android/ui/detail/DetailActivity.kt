d71a2648bc7b56b8616e26ae95b2dfb67b128c70
Tosin <tosinmath007@gmail.com>
2024-11-26 15:02:34 +0000

    Add detail icons

--- androidApp/src/main/java/com/module/notelycompose/android/ui/detail/DetailActivity.kt
+++ androidApp/src/main/java/com/module/notelycompose/android/ui/detail/DetailActivity.kt
@@ -1,22 +1,21 @@
 package com.module.notelycompose.android.ui.detail
 
-import com.module.notelycompose.android.ui.ContentBottomAppBar
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.BottomAppBar
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
@@ -37,23 +36,13 @@ import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.module.notelycompose.android.R
 import com.module.notelycompose.android.theme.MyApplicationTheme
-import com.module.notelycompose.core.DateTimeUtil
-import com.module.notelycompose.notes.domain.Note
-import com.module.notelycompose.notes.presentation.list.ui.NoteList
 import dagger.hilt.android.AndroidEntryPoint
-import java.time.Clock
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.util.TimeZone
 
 @AndroidEntryPoint
 class DetailActivity : ComponentActivity() {
@@ -99,47 +88,54 @@ fun NoteDetailScreen() {
             BottomNavigationBar()
         },
         content = { paddingValues ->
-            Column(
+            Box(
                 modifier = Modifier
+                    .fillMaxSize()
                     .padding(paddingValues)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalAlignment = Alignment.Start
+                    .verticalScroll(rememberScrollState())
             ) {
-                Text(
-                    text = "Milan",
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
+                Column(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.Start
                 ) {
-                    IconButton(onClick = { /* Play audio */ }) {
-                        Icon(imageVector = Icons.Filled.PlayArrow, contentDescription = "Play")
+                    Text(
+                        text = "Milan",
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(onClick = { /* Play audio */ }) {
+                            Icon(imageVector = Icons.Filled.PlayArrow, contentDescription = "Play")
+                        }
+                        Text("46 sec")
                     }
-                    Text("46 sec")
-                }
-                Text(
-                    text = "The most beautiful places to visit:",
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-                ListItem(
-                    text = { Text("The Milan Cathedral. It is the third largest cathedral in the world. I need to buy tickets online or booking a guided tour of the cathedral to save a lot of time!!") },
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-                ListItem(
-                    text = { Text("Galleria Vittorio Emanuele II. Just outside the cathedral, on the Piazza del Duomo. There I can find the most famous fashion designers stores including Vuitton and Prada to do a little shopping") },
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-                ListItem(
-                    text = { Text("Santa Maria delle Grazie church The painting of the Last Supper by Leonardo da Vinci. Remember to book this visit in advance because can only visit by appointment and in small groups of twenty people for 15 minutes!!") },
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
+                    Text(
+                        text = "The most beautiful places to visit:",
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                    ListItem(
+                        text = { Text("The Milan Cathedral. It is the third largest cathedral in the world. I need to buy tickets online or booking a guided tour of the cathedral to save a lot of time!!") },
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                    ListItem(
+                        text = { Text("Galleria Vittorio Emanuele II. Just outside the cathedral, on the Piazza del Duomo. There I can find the most famous fashion designers stores including Vuitton and Prada to do a little shopping") },
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                    ListItem(
+                        text = { Text("Santa Maria delle Grazie church The painting of the Last Supper by Leonardo da Vinci. Remember to book this visit in advance because can only visit by appointment and in small groups of twenty people for 15 minutes!!") },
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
 
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Column(modifier = Modifier.align(Alignment.BottomCenter)) {
-                        EditingToolbar()
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Column(modifier = Modifier.align(Alignment.BottomCenter)) {
+                            EditingToolbar()
+                        }
                     }
                 }
             }
@@ -152,24 +148,40 @@ fun BottomNavigationBar() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
+            .padding(8.dp)
+            .padding(start = 8.dp, end = 48.dp),
         horizontalArrangement = Arrangement.SpaceAround,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        IconButton(onClick = { /* Upload */ }) {
-            Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = "Upload")
+        IconButton(onClick = { /* Share */ }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_detail_share),
+                contentDescription = "Share"
+            )
         }
         IconButton(onClick = { /* Delete */ }) {
-            Icon(imageVector = Icons.Filled.Delete, contentDescription = "Delete")
+            Icon(
+                imageVector = Icons.Filled.Delete,
+                contentDescription = "Type"
+            )
         }
-        IconButton(onClick = { /* Text */ }) {
-            Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = "Text")
+        IconButton(onClick = { /* Type */ }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_detail_type),
+                contentDescription = "Type"
+            )
         }
         IconButton(onClick = { /* Undo */ }) {
-            Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = "Undo")
+            Icon(
+                painter = painterResource(id = R.drawable.ic_detail_undo),
+                contentDescription = "Undo"
+            )
         }
         IconButton(onClick = { /* Redo */ }) {
-            Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = "Redo")
+            Icon(
+                painter = painterResource(id = R.drawable.ic_detail_redo),
+                contentDescription = "Redo"
+            )
         }
     }
 }
@@ -179,55 +191,67 @@ fun EditingToolbar() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(16.dp)
+            .background(
+                color = Color.Blue,
+                shape = RoundedCornerShape(16.dp) // Adjust the radius as needed
+            )
+            .clip(RoundedCornerShape(16.dp)), // Ensures the content respects the rounded corners
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
         IconButton(onClick = { /* Handle A click */ }) {
             Icon(
-                imageVector = Icons.Default.CheckCircle,
+                painter = painterResource(id = R.drawable.ic_detail_text_color),
                 contentDescription = "Align Left",
                 tint = Color.LightGray
             )
         }
         IconButton(onClick = { /* Handle B click */ }) {
             Icon(
-                imageVector = Icons.Default.CheckCircle,
+                painter = painterResource(id = R.drawable.ic_detail_bold),
                 contentDescription = "Bold",
                 tint = Color.LightGray
             )
         }
         IconButton(onClick = { /* Handle I click */ }) {
             Icon(
-                imageVector = Icons.Default.CheckCircle,
+                painter = painterResource(id = R.drawable.ic_detail_italic),
                 contentDescription = "Italic",
                 tint = Color.LightGray
             )
         }
         IconButton(onClick = { /* Handle U click */ }) {
             Icon(
-                imageVector = Icons.Default.CheckCircle,
+                painter = painterResource(id = R.drawable.ic_detail_underline),
                 contentDescription = "Underline",
                 tint = Color.LightGray
             )
         }
         IconButton(onClick = { /* Handle List click */ }) {
             Icon(
-                imageVector = Icons.Default.CheckCircle,
+                painter = painterResource(id = R.drawable.ic_detail_list),
                 contentDescription = "List",
                 tint = Color.LightGray
             )
         }
         IconButton(onClick = { /* Handle Align Center click */ }) {
             Icon(
-                imageVector = Icons.Default.CheckCircle,
+                painter = painterResource(id = R.drawable.ic_detail_align_left),
                 contentDescription = "Align Center",
                 tint = Color.LightGray
             )
         }
         IconButton(onClick = { /* Handle Align Right click */ }) {
             Icon(
-                imageVector = Icons.Default.CheckCircle,
+                painter = painterResource(id = R.drawable.ic_detail_align_center),
+                contentDescription = "Align Right",
+                tint = Color.LightGray
+            )
+        }
+        IconButton(onClick = { /* Handle Align Right click */ }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_detail_align_right),
                 contentDescription = "Align Right",
                 tint = Color.LightGray
             )
