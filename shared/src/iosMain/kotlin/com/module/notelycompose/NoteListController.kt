dded69e8525c90045f4998ce59adecf0c4a0b7c9
Tosin <tosinmath007@gmail.com>
2024-12-14 10:14:45 +0000

    Use Application theme for iOS

--- shared/src/iosMain/kotlin/com/module/notelycompose/NoteListController.kt
+++ shared/src/iosMain/kotlin/com/module/notelycompose/NoteListController.kt
@@ -10,6 +10,7 @@ import com.module.notelycompose.notes.presentation.detail.userinterface.NoteDeta
 import com.module.notelycompose.notes.presentation.list.NoteListEvent
 //import com.module.notelycompose.notes.presentation.list.ui.SharedNoteListScreen
 import com.module.notelycompose.notes.presentation.list.userinterface.SharedNoteListScreen2
+import com.module.notelycompose.notes.presentation.theme.MyApplicationTheme
 import kotlinx.coroutines.DelicateCoroutinesApi
 
 @OptIn(DelicateCoroutinesApi::class)
@@ -17,19 +18,21 @@ fun NoteListController(
     onFloatingActionButtonClicked: () -> Unit,
     onNoteClicked: (Int) -> Unit,
 ) = ComposeUIViewController {
-    val appModule = remember { AppModule() }
-    val viewmodel = remember {
-        IOSNoteListViewModel(
-            getAllNotesUseCase = appModule.getAllNotesUseCase,
-            insertNoteUseCase = appModule.insertNote,
-            deleteNoteById = appModule.deleteNoteById
-        )
-    }
-    val state = viewmodel.state.collectAsState()
-    SharedNoteListScreen2(
-        state.value, onFloatingActionButtonClicked, onNoteClicked
-    ) { id ->
-        viewmodel.onEvent(NoteListEvent.OnNoteDeleted(id))
+    MyApplicationTheme {
+        val appModule = remember { AppModule() }
+        val viewmodel = remember {
+            IOSNoteListViewModel(
+                getAllNotesUseCase = appModule.getAllNotesUseCase,
+                insertNoteUseCase = appModule.insertNote,
+                deleteNoteById = appModule.deleteNoteById
+            )
+        }
+        val state = viewmodel.state.collectAsState()
+        SharedNoteListScreen2(
+            state.value, onFloatingActionButtonClicked, onNoteClicked
+        ) { id ->
+            viewmodel.onEvent(NoteListEvent.OnNoteDeleted(id))
+        }
     }
 }
 
@@ -37,30 +40,32 @@ fun NoteDetailController(
     noteId: String? = null,
     onSaveClicked: () -> Unit
 ) = ComposeUIViewController {
-    val appModule = AppModule()
-    val viewModel = remember {
-        IOSNoteDetailViewModel(
-            getNoteByIdUseCase = appModule.getNoteById,
-            deleteNoteUseCase = appModule.deleteNoteById,
-            insertNoteUseCase = appModule.insertNote
-        )
-    }
-
-    val note : Note? = remember {
-        if (noteId != null) viewModel.getNoteById(noteId) else null
-    }
-
-    NoteDetailScreen(
-        title = note?.title,
-        content = note?.content
-    ) { title, content, isUpdate ->
-        if (isUpdate) {
-            val note = viewModel.getNoteById(note!!.id.toString())
-            viewModel.onEvent(NoteDetailScreenEvent.DeleteNote(note!!.id.toString()))
-            viewModel.onEvent(NoteDetailScreenEvent.NoteSaved(title, content))
-        } else {
-            viewModel.onEvent(NoteDetailScreenEvent.NoteSaved(title, content))
+    MyApplicationTheme {
+        val appModule = AppModule()
+        val viewModel = remember {
+            IOSNoteDetailViewModel(
+                getNoteByIdUseCase = appModule.getNoteById,
+                deleteNoteUseCase = appModule.deleteNoteById,
+                insertNoteUseCase = appModule.insertNote
+            )
         }
-        onSaveClicked()
+
+        val note: Note? = remember {
+            if (noteId != null) viewModel.getNoteById(noteId) else null
+        }
+
+        NoteDetailScreen(
+            title = note?.title,
+            content = note?.content
+        ) { title, content, isUpdate ->
+            if (isUpdate) {
+                val note = viewModel.getNoteById(note!!.id.toString())
+                viewModel.onEvent(NoteDetailScreenEvent.DeleteNote(note!!.id.toString()))
+                viewModel.onEvent(NoteDetailScreenEvent.NoteSaved(title, content))
+            } else {
+                viewModel.onEvent(NoteDetailScreenEvent.NoteSaved(title, content))
+            }
+            onSaveClicked()
+        }
     }
 }
