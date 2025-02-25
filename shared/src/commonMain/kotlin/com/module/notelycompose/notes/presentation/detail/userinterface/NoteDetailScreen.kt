79d9ed52ddbacdd6a9d87515f9323bc0966db741
Tosin <tosinmath007@gmail.com>
2025-02-25 03:04:08 +0000

    Extract Detail Note TopAppBar to separate file

--- shared/src/commonMain/kotlin/com/module/notelycompose/notes/presentation/detail/userinterface/NoteDetailScreen.kt
+++ shared/src/commonMain/kotlin/com/module/notelycompose/notes/presentation/detail/userinterface/NoteDetailScreen.kt
@@ -2,17 +2,12 @@ package com.module.notelycompose.notes.presentation.detail.userinterface
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
@@ -21,13 +16,8 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -53,9 +43,7 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.module.notelycompose.getPlatform
 import com.module.notelycompose.notes.presentation.theme.LocalCustomColors
-import com.module.notelycompose.resources.vectors.IcChevronLeft
 import com.module.notelycompose.resources.vectors.IcRecorder
 import com.module.notelycompose.resources.vectors.Images
 
@@ -77,44 +65,9 @@ fun NoteDetailScreen(
 
     Scaffold(
         topBar = {
-            if (getPlatform().isAndroid) {
-                TopAppBar(
-                    title = { Text("My Note") },
-                    navigationIcon = {
-                        IconButton(onClick = { onNavigateBack() }) {
-                            Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
-                        }
-                    },
-                    backgroundColor = LocalCustomColors.current.bodyBackgroundColor,
-                    contentColor = LocalCustomColors.current.bodyContentColor
-                )
-            } else {
-                TopAppBar(
-                    title = {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier.clickable {
-                                onNavigateBack()
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Images.Icons.IcChevronLeft,
-                                contentDescription = "Back",
-                                modifier = Modifier.size(28.dp)
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(
-                                text = "Back",
-                                style = MaterialTheme.typography.body1,
-                            )
-                        }
-                    },
-                    contentColor = LocalCustomColors.current.iOSBackButtonColor,
-                    backgroundColor = LocalCustomColors.current.transparentColor,
-                    modifier = Modifier.padding(start = 0.dp),
-                    elevation = 0.dp,
-                )
-            }
+            DetailNoteTopBar(
+                onNavigateBack = onNavigateBack
+            )
         },
         floatingActionButton = {
             FloatingActionButton(
