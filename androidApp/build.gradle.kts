f5ce9d1769d8df939e3651a8c6140410da9b4938
Tosin <tosinmath007@gmail.com>
2025-04-22 14:19:22 +0100

    Version 1.0.3

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.module.notelycompose.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
     buildFeatures {
         compose = true
