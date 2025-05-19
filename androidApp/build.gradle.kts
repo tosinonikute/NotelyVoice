271ec6e561eec6202cca48adc1a1b4d278e99546
Tosin <tosinmath007@gmail.com>
2025-05-19 12:03:16 +0100

    Update App version to 1.0.6

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 6
-        versionName = "1.0.5"
+        versionName = "1.0.6"
     }
     buildFeatures {
         compose = true
