2f957a0ab0f6cfb867841c26736300bd6d1a91f5
Tosin <tosinmath007@gmail.com>
2026-06-29 10:15:36 +0100

    App version update v1.3.4

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -170,8 +170,8 @@ android {
         applicationId = "com.module.notelycompose.android"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 34
-        versionName = "1.3.3"
+        versionCode = 35
+        versionName = "1.3.4"
     }
     buildFeatures {
         compose = true
